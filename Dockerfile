6e204dec1dde2b17f6bfd6a5e2b9f1247aedd251
Zhichang Yu <yuzhichang@gmail.com>
2024-07-04 23:53:12 +0800

    Added docker-compose.yml and more env variables

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,23 @@
-FROM ubuntu:20.04
+FROM ubuntu:24.04
 
-ARG RUNNER_VERSION="2.294.0"
+ARG RUNNER_VERSION="2.317.0"
 
 # Prevents installdependencies.sh from prompting the user and blocking the image creation
 ARG DEBIAN_FRONTEND=noninteractive
 
-RUN apt update -y && apt upgrade -y && useradd -m docker
+RUN apt update -y && apt upgrade -y && useradd -m alice
 RUN apt install -y --no-install-recommends \
-    curl jq build-essential libssl-dev libffi-dev python3 python3-venv python3-dev python3-pip
+    curl jq build-essential libssl-dev libffi-dev libicu-dev python3 python3-venv python3-dev python3-pip
 
+RUN apt install -y --no-install-recommends \
+    sudo docker.io git gawk sed wget
 
-RUN cd /home/docker && mkdir actions-runner && cd actions-runner \
+RUN cd /home/alice && mkdir actions-runner && cd actions-runner \
     && curl -O -L https://github.com/actions/runner/releases/download/v${RUNNER_VERSION}/actions-runner-linux-x64-${RUNNER_VERSION}.tar.gz \
     && tar xzf ./actions-runner-linux-x64-${RUNNER_VERSION}.tar.gz
 
-RUN chown -R docker ~docker && /home/docker/actions-runner/bin/installdependencies.sh
+RUN chown -R alice ~alice && /home/alice/actions-runner/bin/installdependencies.sh
+RUN echo "alice      ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/alice
 
 COPY start.sh start.sh
 
@@ -22,7 +25,8 @@ COPY start.sh start.sh
 RUN chmod +x start.sh
 
 # since the config and run script for actions are not allowed to be run by root,
-# set the user to "docker" so all subsequent commands are run as the docker user
-USER docker
+# set the user to "alice" so all subsequent commands are run as the alice user
+USER alice
+ENV USER=alice
 
 ENTRYPOINT ["./start.sh"]
--- start.sh
+++ start.sh
@@ -2,19 +2,36 @@
 
 REPOSITORY=$REPO
 ACCESS_TOKEN=$TOKEN
+RUNNER_NAME=${HOST_HOSTNAME}-$(hostname)
+WORK_DIRECTORY=${WORK_DIRECTORY_PREFIX}/${RUNNER_NAME}
 
 echo "REPO ${REPOSITORY}"
 echo "ACCESS_TOKEN ${ACCESS_TOKEN}"
+echo "EXTRA_LABELS ${EXTRA_LABELS}"
+echo "WORK_DIRECTORY ${WORK_DIRECTORY}"
 
-REG_TOKEN=$(curl -X POST -H "Authorization: token ${ACCESS_TOKEN}" -H "Accept: application/vnd.github+json" https://api.github.com/repos/${REPOSITORY}/actions/runners/registration-token | jq .token --raw-output)
+# https://docs.github.com/en/rest/actions/self-hosted-runners?apiVersion=2022-11-28#create-a-registration-token-for-an-organization
+# https://docs.github.com/en/rest/actions/self-hosted-runners?apiVersion=2022-11-28#create-a-registration-token-for-a-repository
+#https://docs.github.com/en/rest/actions/self-hosted-runners?apiVersion=2022-11-28#delete-a-self-hosted-runner-from-an-organization
+# https://docs.github.com/en/rest/actions/self-hosted-runners?apiVersion=2022-11-28#delete-a-self-hosted-runner-from-a-repository
+if [[ "${REPOSITORY}" =~ / ]]; then
+    ENTITY="repos/${REPOSITORY}"
+else
+    ENTITY="orgs/${REPOSITORY}"
+fi
+REG_TOKEN=$(curl -s -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer ${ACCESS_TOKEN}" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/${ENTITY}/actions/runners/registration-token | jq .token --raw-output)
 
-cd /home/docker/actions-runner
+cd $HOME/actions-runner
+sudo mkdir ${WORK_DIRECTORY} && sudo chown -R ${USER} ${WORK_DIRECTORY}
 
-./config.sh --url https://github.com/${REPOSITORY} --token ${REG_TOKEN}
+LABELS_OPT=${EXTRA_LABELS:+--labels $EXTRA_LABELS}
+./config.sh --url https://github.com/${REPOSITORY} --token ${REG_TOKEN} --name ${RUNNER_NAME} ${LABELS_OPT} --work ${WORK_DIRECTORY}
 
 cleanup() {
     echo "Removing runner..."
-    ./config.sh remove --unattended --token ${REG_TOKEN}
+    REMOVE_TOKEN=$(curl -s -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer ${ACCESS_TOKEN}" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/${ENTITY}/actions/runners/remove-token | jq .token --raw-output)
+    ./config.sh remove --token ${REMOVE_TOKEN}
+    sudo rm -rf ${WORK_DIRECTORY}
 }
 
 trap 'cleanup; exit 130' INT
