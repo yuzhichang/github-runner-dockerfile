0769cd8115ded63e2e5f61cbf12c43716eb3ee5f
Zhichang Yu <yuzhichang@gmail.com>
2024-07-06 13:00:29 +0800

    Export CPUS env

--- start.sh
+++ start.sh
@@ -3,12 +3,20 @@
 REPOSITORY=$REPO
 ACCESS_TOKEN=$TOKEN
 RUNNER_NAME=${HOST_HOSTNAME}-$(hostname)
-WORK_DIRECTORY=${WORK_DIRECTORY_PREFIX}/${RUNNER_NAME}
+RUNNER_WORKSPACE=${RUNNER_WORKSPACE_PREFIX}/${RUNNER_NAME}
+
+NanoCpus=$(sudo docker inspect $(hostname) | jq -r .[0].HostConfig.NanoCpus)
+if [ "$NanoCpus" -eq 0 ]; then
+    export CPUS=$(nproc)
+else
+    export CPUS=$(( (NanoCpus + 999999999) / 1000000000 ))
+fi
 
 echo "REPO ${REPOSITORY}"
 echo "ACCESS_TOKEN ${ACCESS_TOKEN}"
 echo "EXTRA_LABELS ${EXTRA_LABELS}"
-echo "WORK_DIRECTORY ${WORK_DIRECTORY}"
+echo "RUNNER_WORKSPACE ${RUNNER_WORKSPACE}"
+echo "CPUS ${CPUS}"
 
 # https://docs.github.com/en/rest/actions/self-hosted-runners?apiVersion=2022-11-28#create-a-registration-token-for-an-organization
 # https://docs.github.com/en/rest/actions/self-hosted-runners?apiVersion=2022-11-28#create-a-registration-token-for-a-repository
@@ -22,16 +30,16 @@ fi
 REG_TOKEN=$(curl -s -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer ${ACCESS_TOKEN}" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/${ENTITY}/actions/runners/registration-token | jq .token --raw-output)
 
 cd $HOME/actions-runner
-sudo mkdir ${WORK_DIRECTORY} && sudo chown -R ${USER} ${WORK_DIRECTORY}
+sudo mkdir ${RUNNER_WORKSPACE} && sudo chown -R ${USER} ${RUNNER_WORKSPACE}
 
 LABELS_OPT=${EXTRA_LABELS:+--labels $EXTRA_LABELS}
-./config.sh --url https://github.com/${REPOSITORY} --token ${REG_TOKEN} --name ${RUNNER_NAME} ${LABELS_OPT} --work ${WORK_DIRECTORY}
+./config.sh --url https://github.com/${REPOSITORY} --token ${REG_TOKEN} --name ${RUNNER_NAME} ${LABELS_OPT} --work ${RUNNER_WORKSPACE}
 
 cleanup() {
     echo "Removing runner..."
     REMOVE_TOKEN=$(curl -s -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer ${ACCESS_TOKEN}" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/${ENTITY}/actions/runners/remove-token | jq .token --raw-output)
     ./config.sh remove --token ${REMOVE_TOKEN}
-    sudo rm -rf ${WORK_DIRECTORY}
+    sudo rm -rf ${RUNNER_WORKSPACE}
 }
 
 trap 'cleanup; exit 130' INT
